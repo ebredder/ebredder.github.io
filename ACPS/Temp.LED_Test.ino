149d3375a7c3b7a4ac742cd2a99284efe4541bbb
eric <ebredder@pvcc.edu>
2018-04-25 21:12:59 -0400

    .

---
+++ ACPS/Temp.LED_Test.ino
@@ -0,0 +1,33 @@
+#include "DHT.h"
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+int ledpin = 7;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Temperature and Humidty Roxxxxxxxx");
+  
+  dht.begin();
+  pinMode(OUTPUT, ledpin);
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  Serial.print("Humidty: ");
+  Serial.print(h);
+  Serial.print("Temperature: ");
+  Serial.print(t);
+
+  if (t > 20) {
+    digitalWrite(ledpin, HIGH);
+  }
+  else {
+    digitalWrite(ledpin, LOW);
+  }
+  
+  delay(2000);
+}
