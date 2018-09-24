3acff564c3884443165624e06c253cb7be6d73a7
ebredder@pvcc.edu <ebredder@pvcc.edu>
2018-09-23 20:58:56 -0400

    .

---
+++ ETR237/dc/dc_motor_on.ino
@@ -0,0 +1,12 @@
+int motor = 13;
+
+void setup() {                
+  pinMode(motor, OUTPUT);     
+}
+
+void loop() {
+  digitalWrite(motor, HIGH);   
+  delay(3000);
+  digitalWrite(motor, LOW);
+  delay(1500);               
+}
