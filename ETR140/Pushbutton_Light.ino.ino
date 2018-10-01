9852e7ba3b8a6c9f7bbe3f6fb534c9d2773ea048
ebredder@pvcc.edu <ebredder@pvcc.edu>
2018-10-01 02:43:58 -0400

    .

---
+++ ETR140/Pushbutton_Light.ino.ino
@@ -0,0 +1,18 @@
+int inputButton = 2;
+int ledPin = 13;
+
+int inputState = 0;
+
+void setup() {
+  pinMode(inputButton, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  inputState = digitalRead(inputButton);
+
+  if (inputButton == HIGH){
+    digitalWrite(ledPin, HIGH);
+  }
+  digitalWrite(ledPin, LOW);
+}
