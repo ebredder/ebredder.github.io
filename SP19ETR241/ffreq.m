96ed069226ead798e051fedb85e318128cf8bb39
ebredder@pvcc.edu <ebredder@pvcc.edu>
2019-02-25 16:07:16 -0500

    .

--- SP19ETR241/ffreq.m
+++ SP19ETR241/ffreq.m
@@ -1,119 +1,61 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Object not found! : Stanford University</title>
-<script type="text/javascript">
-<!--
-if ((window.screen.width < 640) || (window.screen.height < 640)){document.write('<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1">')}
-//-->
-</script>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<link rel="shortcut icon" type="image/x-icon" href="//www.stanford.edu/favicon.ico">
-<link rel="stylesheet" type="text/css" href="//www.stanford.edu/su-identity/css/su-identity.css" media="all">
-<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" media="all">
-<link rel="stylesheet" type="text/css" href="//www.stanford.edu/stanfordmodern/v2.5/css/stanfordmodern.css" media="all">
-<link rel="stylesheet" type="text/css" href="//www.stanford.edu/stanfordmodern/v2.5/css/mobile.css" media="only screen and (max-width: 640px)">
-<link rel="stylesheet" type="text/css" href="//www.stanford.edu/stanfordmodern/v2.5/css/print.css" media="print">
-<!--[if IEMobile]>
-<link rel="stylesheet" type="text/css" href="//www.stanford.edu/stanfordmodern/v2.5/css/mobile.css" media="screen">
-<link rel="stylesheet" type="text/css" href="//www.stanford.edu/stanfordmodern/v2.5/css/wp7.css" media="screen">
-<![endif]-->
-<script type="text/javascript" src="//www.stanford.edu/stanfordmodern/jquery/jquery-1.8.3.min.js"></script>
-<script type="text/javascript" src="//www.stanford.edu/stanfordmodern/v2.5/js/stanfordmodern.js"></script>
-</head>
-<!-- Stanford Modern Templates Version 2.5 -->
-<body class="sidebar-right">
-<div id="skipnav"><a href="#content">Skip navigation</a></div>
-<div id="su-wrap"> <!-- #su-wrap start -->
-  <div id="su-content"> <!-- #su-content start --> 
-    <!-- Start #layout -->
-    <div id="layout"> 
-      <!-- Start #wrapper -->
-      <div id="wrapper"> 
-        <!-- Start #header -->
-        <div id="header">
-          <div class="container">
-            <div id="logo"><a href="http://www.stanford.edu"><img src="//www.stanford.edu/su-identity/images/stanford-white@2x.png" alt="Stanford" width="160" height="34"></a></div>
-            <div id="search">
-              <form id="cse-search-box" action="http://www.stanford.edu/search">
-                <div class="searchbox">
-                  <input name="cx" type="hidden" value="003265255082301896483:sq5n7qoyfh8">
-                  <input name="cof" type="hidden" value="FORID:9">
-                  <input name="ie" type="hidden" value="UTF-8">
-                  <input name="q" type="text">
-                  <input class="form-submit" name="sa" type="submit" value="Search">
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-        <!-- End #header --> 
-		<!-- Start #container -->
-        <div id="container"> 
-          <!-- Start #content -->
-          <div id="content"> 
-            <!-- Start #center -->
-            <div id="center">
-               <h1>Object not found!</h1>
+function [ f ] = ffreq( x, fs, fr, dt, sq )
+%
+%  f = ffreq(x, fs, fr, dt)
+%
+%   Find frequencies in an RF waveform x
+%
+%   Input:
+%       x -- input RF waveform
+%       fs -- sampling frequency
+%       fr -- frequency resolution
+%       dt -- time sampling period
+%       sq -- threshold value
+%
+%   Output:
+%       f -- frequencies found in signal
+%
 
+% assume reasonable defaults
+if nargin < 5,
+   sq = 0.1;
+end
+if nargin < 4,
+   dt = 0.1;
+end
+if nargin < 3,
+    fr = 1000;
+end
+if nargin < 2,
+    fs = 2048000;
+end
 
-    The requested URL was not found on this server.
+% find the frame size
+nf = fs/fr;
 
-  [an error occurred while processing this directive]
+% find the number of frames to skip
+nt = fs*dt;
 
-    The link on the
-    <a href="http://web.stanford.edu/class/ee179/labs/Lab3.html">referring
-    page</a> seems to be wrong or outdated. Please inform the author of
-    <a href="http://web.stanford.edu/class/ee179/labs/Lab3.html">that page</a>
-    about the error.
+% compute the spectrum of the first block
+xf = fftshift(fft(x(1:nf))).';
 
-  
+% compute the next blocks, spaced by dt seconds
+jj = 1;
+while jj< length(x)-nf,
+    xf = [xf; fftshift(fft(x(jj+(0:nf-1)))).'];
+    jj = jj+nt;
+end
 
-            </div>
-            <!-- End #center --> 
-			
-            <!-- Start #sidebar-right (Removable) -->
-            <div id="sidebar-right" class="sidebar"> </div>
-            <!-- End #sidebar-right (Removable) -->
-			
-            <div class="content_clear"></div>
-          </div>
-          <!-- End #content --> 
-        </div>
-        <!-- End #container --> 
-      </div>
-      <!-- End #wrapper --> 
-    </div>
-    <!-- End #layout --> 
-  </div>
-  <!-- #su-content end --> 
-</div>
-<!-- #su-wrap end --> 
+% find the maximum signals in each frequency bin
+xfm = max(abs(xf));
 
-<!-- Global footer snippet start -->
-<div id="global-footer">
-  <div class="container">
-    <div class="row">
-      <div id="bottom-logo" class="span2"> <a href="http://www.stanford.edu"> <img src="//www.stanford.edu/su-identity/images/footer-stanford-logo@2x.png" alt="Stanford University" width="105" height="49"/> </a> </div>
-      <!-- #bottom-logo end -->
-      <div id="bottom-text" class="span10">
-        <ul>
-          <li class="home"><a href="http://www.stanford.edu">SU Home</a></li>
-          <li class="maps alt"><a href="http://visit.stanford.edu/plan/maps.html">Maps &amp; Directions</a></li>
-          <li class="search-stanford"><a href="http://www.stanford.edu/search/">Search Stanford</a></li>
-          <li class="terms alt"><a href="http://www.stanford.edu/site/terms.html">Terms of Use</a></li>
-          <li class="copyright"><a href="http://www.stanford.edu/site/copyright.html">Copyright Complaints</a></li>
-        </ul>
-      </div>
-      <!-- .bottom-text end -->
-      <div class="clear"></div>
-      <p class="copyright vcard">&copy; <span class="fn org">Stanford University</span>, <span class="adr"> <span class="locality">Stanford</span>, <span class="region">California</span> <span class="postal-code">94305</span> </span></p>
-    </div>
-    <!-- .row end --> 
-  </div>
-  <!-- .container end --> 
-</div>
-<!-- global-footer end --> 
-<!-- Global footer snippet end -->
-</body>
-</html>
+% find the maximimum overall, for normalization
+mx = max(xfm);
+
+% find the indexes of the frequency channels that are abouve the
+% squelch threshold.  Indexes are relative to the carrier
+f = find(abs(xfm)>sq*mx) - nf/2;
+
+% convert the indexes to absolute frequencies
+f = f*fr;
+
+end
